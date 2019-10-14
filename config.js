7843485f53fe87c87c95857b1f3f102d2003c82d
nerdyandnatural <srwilliams8678@gmail.com>
2019-10-13 23:58:09 -0400

    update config file

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
-
-  module.exports = {
-    consumer_key:'XXXXXXXXXXXXXXXXXXXXXXXXXXXX',  
-    consumer_secret:'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-    access_token_key:'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',  
-    access_token_secret:'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxxxxx'
+ module.exports = {
+    consumer_key:'lisZg5LrvpWs3QieXADc3eXEY',  
+    consumer_secret:'xZlWMwrv7gWspLT75FlQRlHnkyQGnXUTxa1A59JfkRjEkjc3cs',
+    access_token:'1182396132523151362-VwfAH5F9Dow89PJM1MF1i56Obh92RX',  
+    access_token_secret:'bTPt1ERtmZ51w6uCsWv77cQHhQt7pm6pocTj1jZpTWjKC'
   }
-
