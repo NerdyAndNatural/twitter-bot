f32bf3424d3affc55ba052677118733d1edc38d1
nerdyandnatural <srwilliams8678@gmail.com>
2019-10-14 00:33:46 -0400

    update config and bot file

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
  module.exports = {
-    consumer_key:'lisZg5LrvpWs3QieXADc3eXEY',  
-    consumer_secret:'xZlWMwrv7gWspLT75FlQRlHnkyQGnXUTxa1A59JfkRjEkjc3cs',
-    access_token:'1182396132523151362-VwfAH5F9Dow89PJM1MF1i56Obh92RX',  
-    access_token_secret:'bTPt1ERtmZ51w6uCsWv77cQHhQt7pm6pocTj1jZpTWjKC'
+    consumer_key:'EQH0zALeijBC2aqn8fJWv4VSn',  
+    consumer_secret:'GqXPkbTVAwvCa1pN6WHXBxaK6jUDPnGrq7KiBgher1WClX0lCa',
+    access_token_key:'1182396132523151362-aKdtUZmfbFRIEzf74jbVq7fe7aeKdT',  
+    access_token_secret:'O9MdW3rkpiABBkeUyLTcNqlsSMghYnWRMB5z1yx9jSirM'
   }
