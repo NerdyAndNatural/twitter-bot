673e76247b9ab9916c30f905879f575944b3c8e4
nerdyandnatural <srwilliams8678@gmail.com>
2019-10-14 08:17:03 -0400

    Update bot

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
  module.exports = {
     consumer_key: process.env.BOT_CONSUMER_KEY,
     consumer_secret: process.env.BOT_CONSUMER_SECRET,
-    access_token: process.env.BOT_ACCESS_TOKEN,
+    access_token_key: process.env.BOT_ACCESS_TOKEN,
     access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
   }
