806eb4361969452136185ff7dfbbeb316a74ae1d
nerdyandnatural <srwilliams8678@gmail.com>
2019-10-30 06:21:58 -0400

    initial commit

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
- module.exports = {
-    consumer_key:'EQH0zALeijBC2aqn8fJWv4VSn',  
-    consumer_secret:'GqXPkbTVAwvCa1pN6WHXBxaK6jUDPnGrq7KiBgher1WClX0lCa',
-    access_token_key:'1182396132523151362-aKdtUZmfbFRIEzf74jbVq7fe7aeKdT',  
-    access_token_secret:'O9MdW3rkpiABBkeUyLTcNqlsSMghYnWRMB5z1yx9jSirM'
-  }
+module.exports = {
+  consumer_key:'XXXXXXXXXXXXXXXXXXXXXXXXXXXX',  
+  consumer_secret:'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+  access_token_key:'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',  
+  access_token_secret:'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxxxxx'
+}
